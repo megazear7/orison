878aeb9376d3f65a313196f84a7d9adcbd37bb7a
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-03-29 08:46:43 -0400

    Recursively crawling down the page hierarchy.

--- bin/build.js
+++ bin/build.js
@@ -1,4 +1,33 @@
 import { renderToString } from '@popeindustries/lit-html-server';
 import index from '../src/pages/index.js';
+import fileWalker from './file-walker.js';
+import fs from 'fs';
 
-console.log(renderToString(index));
+fileWalker(__dirname + '/../src/pages',
+  (err, file) => {
+    if (err) throw err;
+    if (file.includes('/layout.js')) return;
+
+    const newPath = '/build' + file
+      .slice(__dirname.length)
+      .slice('/pages'.length)
+      .replace('.js', '.html');
+    const page = renderToString(require(file).default);
+
+    if (!fs.existsSync(__dirname + '/../build')){
+      fs.mkdirSync(__dirname + '/../build');
+    }
+
+    page.then(html => {
+      fs.writeFile(__dirname + '/..' + newPath, html, err => {
+        if (err) console.log(err);
+      });
+    });
+  },
+  (err, directory) => {
+    const newPath = '/build' + directory.slice(__dirname.length).slice('/pages'.length)
+    fs.mkdir(__dirname + '/..' + newPath, err => {
+      if (err) console.log(err);
+    });
+  }
+);
---
+++ bin/file-walker.js
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+
+export default function fileWalker(dir, fileCallback, directoryCallback) {
+  fs.readdir(dir, (err, list) => {
+    if (err) return directoryCallback(err);
+
+    list.forEach(file => {
+      file = path.resolve(dir, file);
+      fs.stat(file, (err, stat) => {
+        if (stat && stat.isDirectory()) {
+          directoryCallback(null, file);
+          fileWalker(file, fileCallback, directoryCallback);
+        } else {
+          fileCallback(null, file);
+        }
+      });
+    });
+  });
+}
