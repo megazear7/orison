b6ba4e24c7498bfdf2a84090c721706834d65b4b
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-03-30 07:01:10 -0400

    Added basic page level markdown support with more descrptive todos on what we need to do next.

--- README.md
+++ README.md
@@ -19,7 +19,9 @@ Requires [Node.js](https://nodejs.org/en/) 11.0.0+
 ### TODO
 
 1. Create a static directory of some sort.
-1. Allow the use of markdown.
+1. Need to provide someway of doing markdown partials. Markdown pages should auto include layouts.
+1. Provide .html pages and partials which are pulled in as strings and rendered as lit-html templates. These .html pages should auto include layouts.
+1. Delete everything in buildDir except the CNAME. Make the file names that get skipped a configurable array.
 1. Update the example /src files to be documentation of this project.
 1. Update the package definition, giving the project a name, and making the /bin/build.js installed as a global cli utility.
 
--- bin/build.js
+++ bin/build.js
@@ -2,48 +2,24 @@ import { renderToString } from '@popeindustries/lit-html-server';
 import index from '../src/pages/index.js';
 import fileWalker from './file-walker.js';
 import fs from 'fs';
+import md from 'markdown-it';
 
-export default function({
-    buildDir = 'docs'
-  } = {}) {
+export default function({ buildDir = 'docs' } = {}) {
   fileWalker(__dirname + '/../src/pages',
     (err, file) => {
       if (err) throw err;
-      if (file.includes('/layout.js')) return;
-
-      const renderers = require(file).default;
-
-      let pages = [ ];
-      if (renderers.constructor.name === 'TemplateResult') {
-        pages.push({
-          path: '/' + buildDir + file.slice(__dirname.length).slice('/pages'.length).replace('.js', '.html'),
-          html: renderToString(renderers)
-        });
-      } else {
-        Object.keys(renderers).forEach(key => {
-          const renderer = renderers[key];
-          let path = '/' + buildDir + file.slice(__dirname.length).slice('/pages'.length);
-          path = path.slice(0, path.lastIndexOf('/'))
-          path = path + '/' + key + '.html';
-
-          pages.push({
-            path: path,
-            html: renderToString(renderer)
-          });
-        });
-      }
 
       if (!fs.existsSync(__dirname + '/../' + buildDir)){
         fs.mkdirSync(__dirname + '/../' + buildDir);
       }
 
-      pages.forEach(page => {
-        page.html.then(html => {
-          fs.writeFile(__dirname + '/..' + page.path, html, err => {
-            if (err) console.log(err);
-          });
-        });
-      });
+      if (file.includes('/layout.js')) {
+        return;
+      } else if (file.endsWith('.md')) {
+        renderMdFile(file);
+      } else if (file.endsWith('.js')) {
+        renderJsFile(file);
+      }
     },
     (err, directory) => {
       const newPath = __dirname + '/../' + buildDir + directory.slice(__dirname.length).slice('/pages'.length)
@@ -54,4 +30,49 @@ export default function({
       }
     }
   );
+
+  function renderMdFile(file) {
+    let markdown = fs.readFileSync(file).toString();
+    let html = md().render(markdown);
+
+    fs.writeFile(__dirname + '/..' + getBuildPath(file), html, err => {
+      if (err) console.log(err);
+    });
+  }
+
+  function renderJsFile(file) {
+    const renderers = require(file).default;
+
+    let pages = [ ];
+    if (renderers.constructor.name === 'TemplateResult') {
+      pages.push({
+        path: getBuildPath(file),
+        html: renderToString(renderers)
+      });
+    } else {
+      Object.keys(renderers).forEach(key => {
+        const renderer = renderers[key];
+        let path = '/' + buildDir + file.slice(__dirname.length).slice('/pages'.length);
+        path = path.slice(0, path.lastIndexOf('/'))
+        path = path + '/' + key + '.html';
+
+        pages.push({
+          path: path,
+          html: renderToString(renderer)
+        });
+      });
+    }
+
+    pages.forEach(page => {
+      page.html.then(html => {
+        fs.writeFile(__dirname + '/..' + page.path, html, err => {
+          if (err) console.log(err);
+        });
+      });
+    });
+  }
+
+  function getBuildPath(file) {
+    return '/' + buildDir + file.slice(__dirname.length).slice('/pages'.length).replace('.js', '.html');
+  }
 }
