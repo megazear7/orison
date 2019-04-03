1922296f8aee50251e1590ce4ebb37dacd62fb6a
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-04-03 06:23:31 -0400

    Using layouts with markdown page rendering.

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ Requires [Node.js](https://nodejs.org/en/) 11.0.0+
 
 ### TODO
 
-1. Need to provide someway of doing markdown partials. Markdown pages should auto include layouts.
+1. Need to provide someway of doing markdown partials.
 1. Provide .html pages and partials which are pulled in as strings and rendered as lit-html templates. These .html pages should auto include layouts.
 1. Delete everything in buildDir except the CNAME. Make the file names that get skipped a configurable array.
 1. Update the example /src files to be documentation of this project.
--- bin/build.js
+++ bin/build.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
-import { renderToString } from '@popeindustries/lit-html-server';
+import { html, renderToString } from '@popeindustries/lit-html-server';
+import { unsafeHTML } from '@popeindustries/lit-html-server/directives/unsafe-html.js';
 import index from '../src/pages/index.js';
 import fileWalker from './file-walker.js';
 import md from 'markdown-it';
@@ -40,11 +41,41 @@ export default function({ buildDir = 'docs' } = {}) {
   );
 
   function renderMdFile(file) {
-    let markdown = fs.readFileSync(file).toString();
-    let html = md().render(markdown);
+    const markdown = fs.readFileSync(file).toString();
+    const htmlString = md().render(markdown);
 
-    fs.writeFile(getBuildFilePath(file), html, err => {
-      if (err) console.log(err);
+    getLayout(file)
+    .then(layout => renderToString(layout.default(html`${unsafeHTML(htmlString)}`)))
+    .then(html => {
+      fs.writeFile(getBuildFilePath(file), html, err => {
+        if (err) console.log(err);
+      });
+    });
+  }
+
+  function getLayout(filePath) {
+    let directory = path.dirname(filePath);
+
+    while (directory != 'src') {
+      let jsLayoutPath = path.join(directory, 'layout.js');
+      let htmlLayoutPath = path.join(directory, 'layout.html');
+
+      if (fs.existsSync(jsLayoutPath)) {
+        return import(jsLayoutPath);
+      } else if (fs.existsSync(htmlLayoutPath)) {
+        return new Promise((resolve, reject) => {
+          fs.readFile(htmlLayoutPath, 'utf8', (err, htmlString) => {
+            if (err) throw err;
+            resolve(html`${unsafeHTML(htmlString)}`);
+          });
+        });
+      } else {
+        directory = path.dirname(directory);
+      }
+    }
+
+    return new Promise((resolve, reject) => {
+      resolve(page => html`${page}`);
     });
   }
 
