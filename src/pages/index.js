8beac70fd459694a36f1a09bff544b3adb7ef805
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-04-08 07:33:31 -0400

    Added icon attribution

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,4 +11,5 @@ export default () => layout(html`
   ${markdown('./src/partials/documentation/pages.md')}
   ${markdown('./src/partials/documentation/partials.md')}
   ${markdown('./src/partials/documentation/layouts.md')}
+  <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 `);
