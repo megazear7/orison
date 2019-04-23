384e3bd1e662d2ba952c9e4fee640136e338786f
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-04-23 07:20:08 -0400

    Moved the blogs array generator into a blogs directory

--- src/pages/blog/blogs.js
+++ src/pages/blog/blogs.js
@@ -1,11 +1,12 @@
 import { html } from '@popeindustries/lit-html-server';
-import { mdString } from '../../bin/orison-esm.js';
-import client from '../contentful.js';
+import { mdString } from '../../../bin/orison-esm.js';
+import client from '../../contentful.js';
 
 export default async () => {
   const entry = await client.getEntry("5yI7Sof8GKPflIWeG2O9RE");
   const entries = await client.getEntries({
-    'content_type': 'blogPost'
+    'content_type': 'blogPost',
+    'fields.tags': 'orisonjs-blog'
   });
 
   return entries.items.map(entry => {
