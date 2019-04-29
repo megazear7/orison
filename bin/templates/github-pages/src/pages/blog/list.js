b6fa5c80b19e86d73721e9aec883556804c7daa2
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-04-29 08:10:42 -0400

    Fixed the template project blog page

--- bin/templates/github-pages/src/pages/blog/list.js
+++ bin/templates/github-pages/src/pages/blog/list.js
@@ -1,6 +1,7 @@
 import { html } from 'orison';
 
-export default context => [{
+export default (context, slug) => {
+  const blogs = [{
 		name: 'blog-a',
 		html: html `<section><p>Blog A</p></section>`,
 	},
@@ -11,5 +12,7 @@ export default context => [{
 	{
 		name: 'blog-c',
 		html: html `<section><p>Blog C</p></section>`,
-	}
-];
+	}];
+
+  return slug ? blogs.find(blog => blog.name === slug).html : blogs;
+};
