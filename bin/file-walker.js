13f35030b7e77666a035b87726b92cd3884eb2d0
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-05-02 22:17:35 -0400

    Added jsdoc for file-walker.js

--- bin/file-walker.js
+++ bin/file-walker.js
@@ -1,6 +1,14 @@
 import fs from 'fs';
 import path from 'path';
 
+/**
+ * Runs either the fileCallback or the directoryCallback on each file and directory under the given directory.
+ * @param dir The directory to recursively walk through.
+ * @param fileCallback The function to call for each file found.
+ * The first parameter to this method is the file path.
+ * @param directoryCallback The function to call for each directory found.
+ * The first parameter to this method is the directory path.
+ */
 export default function fileWalker(dir, fileCallback = function() {}, directoryCallback = function() {}) {
   fs.readdirSync(dir).forEach(file => {
     const filePath = path.resolve(dir, file);
