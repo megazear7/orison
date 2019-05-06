6738e37da7188f4493ce1c9bb2ba7518f391a7ff
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-05-06 07:47:10 -0400

    Fixed template project home page navigation

--- bin/templates/plain/src/partials/nav.js
+++ bin/templates/plain/src/partials/nav.js
@@ -3,7 +3,7 @@ import { html } from 'orison';
 export default (currentPath, root) => html`
   <nav>
     <div>
-      <a href="/" class="${currentPath === '' ? 'active' : ''}">Begin</a>
+      <a href="/" class="${currentPath.length === 1 ? 'active' : ''}">Begin</a>
       ${root.children.map(child => html`
         <a href="${child.path}" class="${currentPath.startsWith(child.path) ? 'active' : ''}">${child.data.title}</a>
       `)}
