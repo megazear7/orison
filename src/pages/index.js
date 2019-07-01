aa2d807641545a16f0b6e3ff515270964b837de3
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-07-01 13:16:22 -0400

    Added a deploy to netlify button to the documentation site instead of just the readme.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,13 @@
 import { html } from '@popeindustries/lit-html-server';
 
 export default context => html`
+  <section>
+    <div>
+      <a href="https://app.netlify.com/start/deploy?repository=https://github.com/megazear7/orison-netlify-starter-kit">
+        <img src="https://www.netlify.com/img/deploy/button.svg" alt="Deploy to Netlify">
+      </a>
+    </div>
+  </section>
   <section>${context.mdFile('./src/partials/documentation/setup.md')}</section>
   <section>${context.mdFile('./src/partials/documentation/getting-started.md')}</section>
   <section>${context.mdFile('./src/partials/documentation/build.md')}</section>
